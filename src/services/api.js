5e02a0c3a9c3024fdc8be96f7686f8761d132150
jhy <idpc8090@gmail.com>
2025-12-05 15:11:06 +0900

    refactor(init)

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,19 @@
+// services/api.js
 import axios from "axios";
+import { ROUTES } from "../constants/routes";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080", // 백엔드 주소
-  withCredentials: true,
+    baseURL: "http://localhost:8080",
+    withCredentials: true,
 });
+
+// Request 인터셉터 - 요청 전 처리
+api.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    // 요청을 보내기 전 발생하는 에러
+    (error) => {
+        return Promise.reject(error);
+    }
+);
